de890ff6f3a7cab747ffd5b1e3c12777fcabd3e2
Narleks <73983592+Narleks@users.noreply.github.com>
2020-12-19 18:04:16 +0300

    Installer finish

--- setup-nuclear.sh
+++ setup-nuclear.sh
@@ -39,12 +39,50 @@ if ! which docker-compose >> /dev/null; then
         ;;
 	esac
 fi
+echo 'Enter the root password for MariaDB';
+read mariadb_password;
+if [ -z "$mariadb_password" ]
+then
+      mariadb_password="nuclear"
+	  echo "The user's password is not specified, it is used \"\033[32m$mariadb_password\033[m\""
+fi
+echo 'Enter the database name for nuclearengine for MariaDB'
+read mariadb_name;
+if [ -z "$mariadb_name" ]
+then
+      mariadb_name="nuclear"
+	  echo "The database name is not specified, then the \"\033[32m$mariadb_name\033[m\""
+fi
 echo "\033[32mPhase 1: \033[33mClone NuclearEngine Docker repository\033[m";
 git clone https://github.com/Narleks/NarleksEngine-docker.git ./nuclear;
 cd ./nuclear;
 ln -s ./public ./app/public;
 echo "\033[32mPhase 1: \033[33mInstall NuclearEngine Docker\033[m";
+echo "MYSQL_ROOT_PASSWORD=$mariadb_password\nMYSQL_DATABASE=$mariadb_name" > ./config.env;
 docker-compose build;
 echo docker-compose up -d >> start.sh;
-echo "\033[32mPhase 1: \033[33mClone NuclearEngine repository\033[m";
-
+echo "\033[32mPhase 2: \033[33mClone NuclearEngine repository\033[m";
+git clone https://github.com/Narleks/NarleksEngine.git ./public/nuclear;
+echo "\033[32mPhase 3: \033[33mSetting up nuclearengine\033[m";
+echo "<?php
+class DB_config
+{
+    public string \$driver = 'mysql';
+    public string \$dbname = '$mariadb_name';
+    public string \$host = 'mariadb';
+    public string \$port = '3306';
+    public string \$user = 'root';
+    public string \$password = '$mariadb_password';
+    public string \$charest = 'utf8';
+}
+" > ./public/nuclear/data/db_config.php
+chmod -R 777 ./public/nuclear/application
+chmod -R 777 ./public/nuclear/data
+chmod -R 777 ./public/nuclear/data/keypair
+chmod -R 777 ./public/nuclear/data/migrations
+chmod -R 777 ./public/nuclear/core/cache
+chmod -R 777 ./public/nuclear/mirror
+chmod -R 777 ./public/nuclear/mirror/images
+chmod -R 777 ./public/nuclear/templates
+chmod -R 777 ./public/nuclear/templates/default
+echo "\033[32mFinish: \033[33mSuccess\033[m";
